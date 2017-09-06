a51b8d7614f34361a6c3c83963c3ddf8b3b98f96
Cory Zue <czue@dimagi.com>
2017-09-06 17:45:41 +0200

    move status to bottom

--- wine/admin.py
+++ wine/admin.py
@@ -67,11 +67,12 @@ class MerchantWineInline(admin.StackedInline):
 class WineVintageAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Name',        {'fields': ['wine', 'year']}),
-        ('Category',    {'fields': ['category', 'sub_category', 'blend', 'appellation', 'winemakers', 'status']}),
+        ('Category',    {'fields': ['category', 'sub_category', 'blend', 'appellation', 'winemakers']}),
         ('Attributes',  {'fields': ['sweetness', 'wooded', 'organic', 'contains_sulphites', 'vegetarian']}),
         ('Specs',       {'fields': ['optimal_year_start', 'optimal_year_end', 'temp_min', 'temp_max', 'alcohol_percentage', 'residual_sugar', 'ph', 'total_acidity', 'total_sulphur']}),
         ('Copy',        {'fields': ['notes', 'description']}),
         ('Files',       {'fields': ['image_pack_shot', 'image_label_vertical', 'image_label_horizontal', 'tasting_notes']}),
+        ('Status',      {'fields': ['status']}),
     ]
     inlines = [WineGrapeInline, MerchantWineInline, WineAwardInline, WineFoodPairingInline]
     list_display = ('wine', 'year', 'category', 'sub_category')
