8c7d34ea5c9eed396eddee35a639515c4017d73e
Cory Zue <czue@dimagi.com>
2017-09-07 13:40:42 +0200

    support numbers near 0 and add test

--- wine/geoposition.py
+++ wine/geoposition.py
@@ -9,16 +9,17 @@ def geoposition_to_dms_string(geoposition):
 
 def decimal_to_dms(deg):
     # from http://en.proft.me/2015/09/20/converting-latitude-and-longitude-decimal-values-p/
+    is_positive = deg >= 0
     d = int(deg)
     md = abs(deg - d) * 60
     m = int(md)
     sd = (md - m) * 60
-    return [d, m, sd]
+    return [is_positive, abs(d), m, sd]
 
 
-def format_dms(degrees, minutes, seconds, is_latitude):
+def format_dms(is_positive, degrees, minutes, seconds, is_latitude):
     if is_latitude:
-        prefix = 'N' if degrees > 0 else 'S'
+        prefix = 'N' if is_positive else 'S'
     else:
-        prefix = 'E' if degrees > 0 else 'W'
+        prefix = 'E' if is_positive else 'W'
     return '{} {}° {}\' {:.2f}"'.format(prefix, abs(degrees), minutes, seconds)
--- wine/tests.py
+++ wine/tests.py
@@ -51,3 +51,9 @@ class GeopositionTest(SimpleTestCase):
     def test_simple(self):
         self.assertEqual('''S 33° 49' 20.92" E 18° 55' 48.51"''',
                          geoposition_to_dms_string(Geoposition(-33.8224777, 18.9301428)))
+
+    def test_near_zero(self):
+        self.assertEqual('''S 0° 49' 20.92" W 0° 55' 48.51"''',
+                         geoposition_to_dms_string(Geoposition(-0.8224777, -0.9301428)))
+        self.assertEqual('''N 0° 49' 20.92" E 0° 55' 48.51"''',
+                         geoposition_to_dms_string(Geoposition(0.8224777, 0.9301428)))
