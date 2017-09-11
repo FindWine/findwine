d268df227a4683b4b9a1e9c26e7602983be86b6a
Cory Zue <czue@dimagi.com>
2017-09-11 11:38:28 +0200

    add function and test for url slugs

---
+++ wine/tests/test_url_slugs.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from wine.util import generate_slug_from_parts
+
+
+class WineVintageSlugTest(TestCase):
+
+    def test_formatting(self):
+        TEST_CASES = (
+            (('Bouchard Finlayson', 'Galpin Peak', 2015), 'bouchard-finlayson-galpin-peak-2015'),
+            (("Apostrophe's", "And other spec!@l C#arachters", 2015), 'apostrophes-and-other-specl-carachters-2015'),
+            (("   leading and ", " trailing whitespace    ", 2015), 'leading-and-trailing-whitespace-2015'),
+            ((None, None, 2015), 'unknown-producer-unknown-wine-2015'),
+            (('', '', 2015), 'unknown-producer-unknown-wine-2015'),
+        )
+        for inputs, expected_output in TEST_CASES:
+            self.assertEqual(expected_output, generate_slug_from_parts(*inputs))
---
+++ wine/util.py
@@ -0,0 +1,23 @@
+from django.utils.text import slugify
+
+
+def generate_slug(wine_vintage):
+    """
+    Slug format should be: [producer name]-[wine name]-[winevintage year]
+    """
+    return generate_slug_from_parts(
+        producer_name=wine_vintage.producer.name,
+        wine_name=slugify(wine_vintage.wine.name),
+        year=wine_vintage.year,
+    )
+
+
+def generate_slug_from_parts(producer_name, wine_name, year):
+    producer_name = producer_name or 'Unknown Producer'
+    wine_name = wine_name or 'Unknown Wine'
+    return '{producer}-{wine}-{year}'.format(
+        producer=slugify(producer_name),
+        wine=slugify(wine_name),
+        year=year,
+    )
+
