ae2f2d7e39cfc969f393f76d1ef1bf0cbd77bb6f
Cory Zue <czue@dimagi.com>
2017-09-11 13:44:16 +0200

    make slug not null and not editable

--- wine/models.py
+++ wine/models.py
@@ -117,7 +117,7 @@ class Winemaker(models.Model):
 
 
 class WineVintage(models.Model):
-    slug = models.CharField(max_length=255, unique=True, null=True, blank=True)
+    slug = models.CharField(max_length=255, unique=True, editable=False)
     wine = models.ForeignKey("Wine")
     category = models.ForeignKey("Category")
     sub_category = models.ForeignKey("SubCategory")
