619615548847c1f772c594b973d04cadbe8eb599
Cory Zue <czue@dimagi.com>
2017-09-12 10:01:47 +0200

    remove old wine URL

--- wine/urls.py
+++ wine/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 
 from . import views
 
+
 app_name = 'wine'
 urlpatterns = [
     # ex: /
@@ -12,11 +13,10 @@ urlpatterns = [
     url(r'^search/$', views.ResultsView.as_view(), name='results'),
     # ex: /index/
     url(r'^index/$', views.IndexView.as_view(), name='index'),
-    # ex: /wine/2/  
-    url(r'^wine/(?P<pk>[0-9]+)/$', views.WineDetailView.as_view(), name='wine_detail'),
+    # ex: /wine/paul-cluver-noble-late-harvest-2014/
     url(r'^wine/(?P<slug>[A-Za-z0-9_-]+)/$', views.WineDetailView.as_view(), name='wine_detail_by_slug'),
     # ex: /advanced_search/
     url(r'^advanced_search/$', views.AdvancedSearch, name='advanced_search'),
-    # ex: /producer/3/  
+    # ex: /producer/3/
     url(r'^producer/(?P<pk>[0-9]+)/$', views.ProducerDetailView.as_view(), name='producer_detail'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # NOT SUITABLE FOR PRODCUTION
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # NOT SUITABLE FOR PRODCUTION
