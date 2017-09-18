a21e2f095f689fcc5f6cabaf659f48d7c0be01d1
Cory Zue <czue@dimagi.com>
2017-09-18 14:55:00 +0200

    fix dangling references to short_name

--- wine/admin.py
+++ wine/admin.py
@@ -77,7 +77,7 @@ class WineVintageAdmin(admin.ModelAdmin):
     inlines = [WineGrapeInline, MerchantWineInline, WineAwardInline, WineFoodPairingInline]
     list_display = ('wine', 'year', 'category', 'sub_category', 'status')
     list_filter = ('status', )
-    search_fields = ['wine__short_name', 'wine__producer__name']
+    search_fields = ['wine__name', 'wine__producer__name']
 
     def view_on_site(self, obj):
         return reverse('wine:wine_detail_by_slug', kwargs={'slug': obj.slug})
