4d3ff114a2d3110a63ad8b72c850e739b18d7e39
Cory Zue <czue@dimagi.com>
2017-10-17 10:04:44 +0200

    delete old advanced search and search result views/urls

--- wine/urls.py
+++ wine/urls.py
@@ -10,14 +10,10 @@ app_name = 'wine'
 urlpatterns = [
     # ex: /
     url(r'^$', views.search, name='search'),
-    # ex: /search/
-    url(r'^search/$', views.ResultsView.as_view(), name='results'),
     # ex: /index/
     url(r'^index/$', views.IndexView.as_view(), name='index'),
     # ex: /wine/paul-cluver-noble-late-harvest-2014/
     url(r'^wine/(?P<slug>[A-Za-z0-9_-]+)/$', views.WineDetailView.as_view(), name='wine_detail_by_slug'),
-    # ex: /advanced_search/
-    url(r'^advanced_search/$', views.AdvancedSearch, name='advanced_search'),
     # ex: /producer/3/
     url(r'^producer/(?P<pk>[0-9]+)/$', views.ProducerDetailView.as_view(), name='producer_detail'),
     url(r'^500/$', views.error, name='simulate_error'),
--- wine/views.py
+++ wine/views.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 import json
 
-from django.db.models import Avg, Min, Func
+from django.db.models import Func
 from django.shortcuts import render
 from django.views import generic
 from django.views.decorators.http import require_GET
 
-from .models import WineVintage, MerchantWine, Producer, Category
-from .forms import AdvancedSearchForm
+from .models import WineVintage, Producer, Category
 
 
 class IndexView(generic.ListView):
@@ -23,7 +22,7 @@ class IndexView(generic.ListView):
 def search(request):
     category_list = list(Category.objects.values_list('name', flat=True))
     category_map = _build_category_subcategory_mapping()
-    return render(request, 'wine/search_new.html', {
+    return render(request, 'wine/search.html', {
         'categories': json.dumps(category_list),
         'category_mapping': json.dumps(category_map),
     })
@@ -40,50 +39,6 @@ def _build_category_subcategory_mapping():
     }
 
 
-class ResultsView(generic.ListView):
-    template_name = 'wine/results.html'
-    context_object_name = 'results_list'
-    paginate_by = 50
-
-    def get_queryset(self):
-        category = self.request.GET.get('category', '')
-        sub_category = self.request.GET.get('sub_category', '')
-        min_price = self.request.GET.get('min_price', '')
-        max_price = self.request.GET.get('max_price', '')
-        #1 todo: Add country
-        results_list = MerchantWine.objects.filter(
-            #2 Restrict MerchantWines to those that are available
-            available__exact="True",
-            #3 Restrict MerchantWines to those that are above or equal to the minimum price
-            price__gte=min_price,
-            #4 Restrict MerchantWines to those that are below or equal to the maximum price
-            price__lte=max_price,
-            #5 Restrict MerchantWines to the selected category
-            wine_vintage__category__name__exact=category,
-        )
-
-        #6 Restrict MerchantWines to the selected sub_category or leave if no sub_category is selected NB! UPDATE FOR MULTIPLE SELECT
-        if sub_category != 'All':
-            results_list = results_list.filter(wine_vintage__sub_category__name__exact=sub_category)
-
-        #7 Restrict MerchantWines to lowest price version(s) of same wine_vintage
-        wines = WineVintage.objects.distinct().filter(merchantwine__in=results_list)
-        #8 Only show lowest price and add average rating
-        wines = wines.annotate(
-            price=Round(Min('merchantwine__price')),
-            avg_rating=Round(Avg('wineaward__award__tier__normalised_rating')),
-
-        )
-        wines = wines.order_by(
-            '-avg_rating', 'price'
-        )
-        # todo: if necessary
-        # #9 Restrict MerchantWines to lowest minimum_purchase_unit of same wine_vintage, price and merchant
-        # # NOT WORKING min_units = results_list.values('wine_vintage').annotate(smallest_unit=Min('minimum_purchase_unit'))
-        # # NOT WORKING results_list = results_list.filter(minimum_purchase_unit__in=min_units.values('smallest_unit'))
-        return wines
-
-
 class WineDetailView(generic.DetailView):
     model = WineVintage
     template_name = 'wine/wine_detail.html'
@@ -97,21 +52,6 @@ class ProducerDetailView(generic.DetailView):
 class Round(Func):
     function = 'ROUND'
     template = '%(function)s(%(expressions)s, 1)'
-
-
-def AdvancedSearch(request):
-    # REMOVE POST STUFF? ADD???
-    if request.method == 'POST':
-        form = AdvancedSearchForm(request.POST)
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            category = form.cleaned_data['category']
-            sub_category = form.cleaned_data['sub_category']
-            # redirect to a new URL:
-            return render(request, 'wine/search.html', {'form': form})
-    else:
-        form = AdvancedSearchForm()
-        return render(request, 'wine/search.html', {'form': form})
 
 
 @require_GET
