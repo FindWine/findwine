1f09e4c1f8a14f25863ca38c0bd90178bf515624
Cory Zue <czue@dimagi.com>
2017-12-14 13:39:44 +0200

    move stuff around

--- integrations/data_cleanup.py
+++ integrations/data_cleanup.py
@@ -1,5 +1,16 @@
 import requests
 from wine.models import MerchantWine
+from django.urls import reverse
+
+
+def clean_invalid_urls_and_notify(debug=False):
+    cleaned_wines = clean_invalid_urls(debug)
+    for merchant_wine in cleaned_wines:
+        print('URL for {} was not valid: {}. This can be fixed here: {}'.format(
+            merchant_wine, merchant_wine.url, _get_admin_url(merchant_wine)
+        ))
+    if not cleaned_wines:
+        print('No invalid urls found! Hooray!')
 
 
 def clean_invalid_urls(debug=False):
@@ -17,3 +28,11 @@ def clean_invalid_urls(debug=False):
 def url_is_valid(url):
     r = requests.get(url)
     return r.status_code == 200
+
+
+def _get_admin_url(model_instance):
+    # ht: https://stackoverflow.com/a/10420949/8207
+    return 'https://www.findwine.com{}'.format(reverse(
+        'admin:{}_{}_change'.format(model_instance._meta.app_label, model_instance._meta.model_name),
+        args=(model_instance.pk,)
+    ))
--- integrations/management/commands/clean_dead_links.py
+++ integrations/management/commands/clean_dead_links.py
@@ -1,6 +1,5 @@
 from django.core.management import BaseCommand
-from django.urls import reverse
-from integrations.data_cleanup import clean_invalid_urls
+from integrations.data_cleanup import clean_invalid_urls_and_notify
 
 
 class Command(BaseCommand):
@@ -16,18 +15,4 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        cleaned_wines = clean_invalid_urls(options['debug'])
-        for merchant_wine in cleaned_wines:
-            print('URL for {} was not valid: {}. This can be fixed here: {}'.format(
-                merchant_wine, merchant_wine.url, _get_admin_url(merchant_wine)
-            ))
-        if not cleaned_wines:
-            print('No invalid urls found! Hooray!')
-
-
-def _get_admin_url(model_instance):
-    # ht: https://stackoverflow.com/a/10420949/8207
-    return 'https://www.findwine.com{}'.format(reverse(
-        'admin:{}_{}_change'.format(model_instance._meta.app_label, model_instance._meta.model_name),
-        args=(model_instance.pk,)
-    ))
+        clean_invalid_urls_and_notify(options['debug'])
