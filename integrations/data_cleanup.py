1c9379e703fb7a4a1c621f6ca525202c8f85655a
Cory Zue <czue@dimagi.com>
2017-12-14 10:53:48 +0200

    add management command to clean dead links

--- integrations/data_cleanup.py
+++ integrations/data_cleanup.py
@@ -1,29 +1,19 @@
-from django.urls import reverse
 import requests
-from wine.context_processors import absolute_url
 from wine.models import MerchantWine
 
 
 def clean_invalid_urls(debug=False):
+    updated_merchants = []
     for merchant_wine in MerchantWine.objects.filter(available=True):
         if not url_is_valid(merchant_wine.url):
             if not debug:
                 merchant_wine.available = False
                 merchant_wine.save()
-            else:
-                print('URL for {} was not valid: {}. This can be fixed here: {}'.format(
-                    merchant_wine, merchant_wine.url, _get_admin_url(merchant_wine)
-                ))
+            updated_merchants.append(merchant_wine)
+
+    return updated_merchants
 
 
 def url_is_valid(url):
     r = requests.get(url)
     return r.status_code == 200
-
-
-def _get_admin_url(model_instance):
-    # ht: https://stackoverflow.com/a/10420949/8207
-    return 'https://www.findwine.com{}'.format(reverse(
-        'admin:{}_{}_change'.format(model_instance._meta.app_label, model_instance._meta.model_name),
-        args=(model_instance.pk,)
-    ))
---
+++ integrations/management/commands/clean_dead_links.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand
+from django.urls import reverse
+from integrations.data_cleanup import clean_invalid_urls
+
+
+class Command(BaseCommand):
+    help = 'Checks all Merchant Wines for dead links and de-activates any that are found.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--debug',
+            action='store_true',
+            dest='debug',
+            default=False,
+            help="Just print information but don't save anything",
+        )
+
+    def handle(self, *args, **options):
+        cleaned_wines = clean_invalid_urls(options['debug'])
+        for merchant_wine in cleaned_wines:
+            print('URL for {} was not valid: {}. This can be fixed here: {}'.format(
+                merchant_wine, merchant_wine.url, _get_admin_url(merchant_wine)
+            ))
+
+
+def _get_admin_url(model_instance):
+    # ht: https://stackoverflow.com/a/10420949/8207
+    return 'https://www.findwine.com{}'.format(reverse(
+        'admin:{}_{}_change'.format(model_instance._meta.app_label, model_instance._meta.model_name),
+        args=(model_instance.pk,)
+    ))
