e3904769f6b53c705002c91a41caa8561b6b6fdc
Cory Zue <czue@dimagi.com>
2018-04-05 14:44:08 +0200

    add basic logic to wire up search controls

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,8 +1,81 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
 
+const queryString = require('query-string');
+
+// todo: figure out how to django-ize these
+const SEARCH_API_URL = '/api/search/';
+
+
+class SearchByNameControl extends React.Component {
+    render () {
+        return (
+            <div>
+                <label htmlFor="search">Search: </label>
+                <input type="text" id="search" placeholder="Warwick" value={this.props.searchText}
+                       onChange={(e) => this.props.searchTextChanged(e.target.value)}/>
+                <button onClick={() => this.props.doSearch()}>Search</button>
+            </div>
+        );
+    }
+}
+
+class SearchResults extends React.Component {
+    render () {
+        return (
+            <ul>
+                <li>todo</li>
+                <li>todo</li>
+                <li>todo</li>
+            </ul>
+        )
+    }
+}
+
+class SearchByNamePage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            searchText: '',
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <SearchByNameControl searchText={this.state.searchText}
+                                     searchTextChanged={(text) => this.searchTextChanged(text)}
+                                     doSearch={() => this.doSearch()}
+                />
+                <SearchResults />
+            </div>
+        )
+    }
+
+    searchTextChanged(text) {
+        this.setState({
+            searchText: text
+        });
+    }
+
+    doSearch() {
+        let params = queryString.stringify({q: this.state.text});
+        console.log(params)
+        fetch(SEARCH_API_URL + '?' + params).then((response) => this._updateResultsFromResponse(response));
+        console.log('search!', this.state.searchText);
+    }
+
+    _updateResultsFromResponse(response) {
+        if (response.ok) {
+            response.json().then((responseJson) => {
+                console.log(responseJson);
+            });
+        }
+    }
+}
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <SearchByNamePage/>,
     document.getElementById('react-home')
 );
