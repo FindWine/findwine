f3fcbca60fe8e988bd876fc8c68fd89a8d56eb9f
Cory Zue <czue@dimagi.com>
2019-07-18 14:00:34 +0200

    add modal option for price widget

--- assets/js/price-widget-v1.js
+++ assets/js/price-widget-v1.js
@@ -42,6 +42,47 @@ window.FindWine = (function() {
         li.findwine-no-data {
             font-size: 1em;
         }
+        
+        .findwine-modal {
+            /* https://www.w3schools.com/howto/howto_css_modals.asp */
+            display: none;
+            position: fixed; /* Stay in place */
+            z-index: 10000; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+        }
+        
+        .findwine-modal.is-open {
+            display: block;
+        }
+        
+        .findwine-modal-content {
+            background-color: #fefefe;
+            margin: 15% auto; /* 15% from the top and centered */
+            padding: 20px;
+            border: 1px solid #888;
+            min-width: 300px; /* Could be more or less, depending on screen size */
+            max-width: 500px; /* Could be more or less, depending on screen size */
+        }
+        .findwine-modal-close {
+          color: #aaa;
+          position: relative;
+          font-size: 2em;
+          font-weight: bold;
+          line-height: 1;
+        }
+        
+        .findwine-modal-close:hover,
+        .findwine-modal-close:focus {
+          color: black;
+          text-decoration: none;
+          cursor: pointer;
+        }
         `;
         // append style to page
         // https://stackoverflow.com/a/524721/8207
@@ -80,7 +121,37 @@ window.FindWine = (function() {
         widgetElement.innerHTML = `<ul class="findwine-buy-list">${priceHtml}</ul>`;
     }
 
+    function renderModal(widgetElement, partnerId, wineJson) {
+        let modalElt = document.createElement('div');
+        modalElt.classList.add('findwine-modal');
+        let modalInner = document.createElement('div');
+        modalInner.classList.add('findwine-modal-content');
+        modalElt.appendChild(modalInner);
+        let closeButton = document.createElement('span');
+        closeButton.classList.add('findwine-modal-close');
+        closeButton.innerHTML = '&times;';
+        closeButton.addEventListener('click', function () {
+            modalElt.classList.remove('is-open');
+        });
+        modalInner.appendChild(closeButton);
+        let prices = document.createElement('div');
+        renderWidget(prices, partnerId, wineJson);
+        modalInner.appendChild(prices);
+        document.body.appendChild(modalElt);
+        if (wineJson.price_data.listings.length === 0) {
+            widgetElement.innerHTML = "<li class='findwine-buy-item findwine-no-data'>Out of Stock</li>";
+        } else {
+            widgetElement.innerHTML = `<button class="findwine-modal-opener">Buy from R${wineJson.price_data.lowest_price}</button>`;
+            widgetElement.addEventListener('click', function () {
+                modalElt.classList.add('is-open');
+            });
+        }
+    }
+
     function init(partnerId) {
+        /*
+        Use inline=true to render the prices directly inline with the widget.
+         */
         addStyles();
         // swap these for development
         // let apiRoot = 'http://localhost:8000/api/wine-prices/';
@@ -93,11 +164,18 @@ window.FindWine = (function() {
             for (let i = 0; i < widgets.length; i++) {
                 let widget = widgets[i];
                 let wineId = widget.dataset.findwineId;
+                // https://stackoverflow.com/a/18007234/8207
+                let isModal = widget.dataset.findwineIsModal !== undefined;
+                console.log("is modal", isModal);
                 let url = `${apiRoot}${wineId}/`;
                 fetch(url).then((response) => {
                     if (response.ok) {
                         response.json().then((responseJson) => {
-                            renderWidget(widget, partnerId, responseJson);
+                            if (isModal) {
+                                renderModal(widget, partnerId, responseJson);
+                            } else {
+                                renderWidget(widget, partnerId, responseJson);
+                            }
                         });
                     }
                 });
